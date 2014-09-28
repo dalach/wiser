4ac22430189af2326d3139afd6aea81517c0a1bc
dalach <wikadalach@gmail.com>
2014-09-28 23:13:07 +0200

    run migration for Authors

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140928154548) do
+ActiveRecord::Schema.define(version: 20140928164550) do
+
+  create_table "authors", force: true do |t|
+    t.string   "first_name"
+    t.string   "family_name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "quotes", force: true do |t|
     t.text     "content"
