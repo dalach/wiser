aa0a21a9ce679d0157cd3610be086302232ac02f
dalach <wikadalach@gmail.com>
2014-09-28 19:09:33 +0200

    add actions to the authors_controller

---
+++ app/controllers/authors_controller.rb
@@ -0,0 +1,54 @@
+class AuthorsController < ApplicationController
+  before_action :set_author, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @authors = Author.all
+  end
+
+  def new
+    @author = Author.new
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def create
+    @author = Author.new(author_params)
+    if @author.save!
+      flash[:notice] = "Author added :)"
+      redirect_to @author
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @author.update(author_params)
+      flash[:notice] = "Author updated :)"
+      redirect_to @author
+    else
+      redirect_to authors_path, error: 'Author was NOT updated.'
+  end
+
+  def destroy
+    if @author.destroy
+      flash[:notice] = "Author destroyed"
+      redirect_to authors_path
+    else
+      redirect_to authors_path, error: "Author was NOT destroyed"
+    end
+  end
+
+  private
+  def set_author
+    @author = Author.find(params[:id])
+  end
+
+  private
+  def author_params
+    params.require(:author).permit(:first_name, :family_name)
+  end
+end
